40efcddad9b5676eea55dbeea60a67f9318efcce
Dmytro Zhelezniak <d.zhelezniak@viseven.com>
2021-09-08 19:03:53 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -2,19 +2,19 @@ const elWrapper = document.querySelector('.wrapper');
 const elContainer = document.querySelector('.container');
 const elBlock = document.querySelector('.block');
 
-elWrapper.addEventListener("click", event => {
-    //event.stopPropagation();
-    console.log('click Wrapper')
-}, true);
-
-elContainer.addEventListener('click', event => {
-    //event.stopPropagation();
-    console.log('click Container')
-}, true);
+// elWrapper.addEventListener("click", event => {
+//     //event.stopPropagation();
+//     console.log('click Wrapper')
+// });
+//
+// elContainer.addEventListener('click', event => {
+//     //event.stopPropagation();
+//     console.log('click Container')
+// });
 
 elBlock.addEventListener('click', event => {
     //event.stopPropagation();
     console.log('click Block')
-}, true);
+});
 
 
